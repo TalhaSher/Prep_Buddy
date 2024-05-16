79c3dda209bc79280bc2238a25a003ea28cf93a5
Talha Sher <xexcalibur9090@gmail.com>
2024-05-17 00:42:52 +0500

    update ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,6 +8,10 @@ import SubjectCarousaleWrapper from "../components/SubjectCarousale";
 import { CarouselTransition } from "../components/Qoutes";
 import Authenticator from "../components/Authenticator";
 
+// Triangle stuff
+
+import { Box } from "@mui/material";
+
 export default function Home() {
   return (
     <Authenticator>
@@ -16,6 +20,51 @@ export default function Home() {
         <Pill />
         <Divider />
         <SubjectCarousaleWrapper />
+        <Divider />
+        <Box
+          width="100%"
+          height="30vh"
+          sx={{
+            display: "flex",
+            gap: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            background: "rgba(248, 255, 253, 0.14)",
+            borderBottomLeftRadius: "16px",
+            borderBottomRightRadius: "16px",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(8.4px)",
+            WebkitBackdropFilter: "blur(8.4px)",
+            border: "1px solid rgba(248, 255, 253, 0.17)",
+          }}
+        >
+          <Box
+            height="150px"
+            width="150px"
+            sx={{
+              backgroundColor: "red",
+              background: "rgba(0, 0, 0, 0.14)",
+              boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+              backdropFilter: "blur(8.4px)",
+              WebkitBackdropFilter: "blur(8.4px)",
+              border: "1px solid rgba(248, 255, 253, 0.17)",
+            }}
+          ></Box>
+          <Box
+            height="150px"
+            width="150px"
+            sx={{
+              backgroundColor: "blue",
+              backgroundColor: "red",
+              background: "rgba(0, 0, 0, 0.14)",
+              boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+              backdropFilter: "blur(8.4px)",
+              WebkitBackdropFilter: "blur(8.4px)",
+              border: "1px solid rgba(248, 255, 253, 0.17)",
+            }}
+          ></Box>
+        </Box>
+
         <Divider className="py-1" />
         <h1
           style={{ fontFamily: "Roboto,Kanit,Nunito", fontSize: "1rem" }}
